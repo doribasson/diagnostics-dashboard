950b0ab565bd9c3e5366825dd72f34c5b969c02a
Dori basson <dorib.api@gmail.com>
2025-06-23 01:13:08 +0300

    fix imports pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import Layout from "./components/Layout/Layout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import "./styles/globals.scss";
 
-const Info = lazy(() => import("./pages/Info"));
-const Notifications = lazy(() => import("./pages/Notifications"));
-const Files = lazy(() => import("./pages/Files"));
-const Settings = lazy(() => import("./pages/Settings"));
-const Logout = lazy(() => import("./pages/Logout"));
+const Info = lazy(() => import("./pages/Info/Info"));
+const Notifications = lazy(() => import("./pages/Notifications/Notifications"));
+const Files = lazy(() => import("./pages/Files/Files"));
+const Settings = lazy(() => import("./pages/Settings/Settings"));
+const Logout = lazy(() => import("./pages/Logout/Logout"));
 
 function App() {
   return (
